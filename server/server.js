de71d63ee26b54bd862921dc376a357f0184beef
tawfiq58 <tawfiqhasan58@gmail.com>
2023-04-07 20:28:16 +0600

    chore: remove console.log statement from server/** directory
    This commit removes the console.log statement that was added to the code in
    the server/** directory to test whether the GitHub workflows were working
    properly. Since the test has been successful, the statement is no longer needed
    and has been removed to keep the code clean and free of unnecessary clutter.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  console.log('Check github workflows...');
   res.send('Hello from server!');
 });
 
